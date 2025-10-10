c58a5e14f5b6f9f771a0429b45dd9880730b5d09
tuan kiet <kietpt301104@gmail.com>
2025-10-11 02:05:21 +0700

    resolve port

--- survey-fe/src/utils/api.js
+++ survey-fe/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: (import.meta.env.VITE_API_URL || "http://localhost:3000").replace(/\/+$/,""),
+  baseURL: (import.meta.env.VITE_API_URL || "http://localhost:80").replace(/\/+$/,""),
 });
 
 api.interceptors.request.use((config) => {
